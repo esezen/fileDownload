1a7b65bd3f255ed9ea98934525fc26b4df719e2e
esezen <eneskutaysezen@gmail.com>
2015-11-15 01:39:03 -0500

    Downloading content

--- src/fileDownloader/downloader.java
+++ src/fileDownloader/downloader.java
@@ -1,11 +1,36 @@
 package fileDownloader;
 
+import java.net.*;
+import java.io.*;
+
 public class downloader {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+	public static void main(String[] args) throws IOException {
+		
+		String fileName = "file.txt";
+		String url = "http://shayconcepts.com";
+		URL link = new URL(url);
+		InputStream in = new BufferedInputStream(link.openStream());
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		byte[] buf = new byte[1024];
+		int n = 0;
+		while (-1!=(n=in.read(buf)))
+		{
+		    out.write(buf, 0, n);
+		}
+		out.close();
+		in.close();
+		byte[] response = out.toByteArray();
+		
+		FileOutputStream fos = new  FileOutputStream(fileName);
+		fos.write(response);
+		fos.close();
+		System.out.println("Done");
+		
 		
 	}
 
 }
+
+		
+
